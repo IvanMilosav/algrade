639d3dfde8eca499c11ed0e375433d09a91715ac
BookStore <ivansmgo@gmail.com>
2021-04-08 18:37:07 +0200

    Updates

---
+++ precache-manifest.39c10b86c33ed4f0dbd5719da52bac5d.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84f954a296bb1adbc9753ae9712c731",
+    "url": "/algrade/index.html"
+  },
+  {
+    "revision": "b25aff48fc6af07965b5",
+    "url": "/algrade/static/css/2.e8266809.chunk.css"
+  },
+  {
+    "revision": "8963cac2ffbbd48a6870",
+    "url": "/algrade/static/css/main.9c190900.chunk.css"
+  },
+  {
+    "revision": "b25aff48fc6af07965b5",
+    "url": "/algrade/static/js/2.5211facc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/algrade/static/js/2.5211facc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8963cac2ffbbd48a6870",
+    "url": "/algrade/static/js/main.6696acbc.chunk.js"
+  },
+  {
+    "revision": "5527f6d628b4a4cd563a",
+    "url": "/algrade/static/js/runtime-main.c427d41f.js"
+  },
+  {
+    "revision": "d4a462442ee5d2bf128135d834a4942e",
+    "url": "/algrade/static/media/arab2.d4a46244.jpg"
+  },
+  {
+    "revision": "e45354cad7cf5b9bbb858d8f0fb26adb",
+    "url": "/algrade/static/media/bathroom.e45354ca.webp"
+  },
+  {
+    "revision": "f856b33f2721a429462405bafbeee04b",
+    "url": "/algrade/static/media/bgImg1u.f856b33f.jpg"
+  },
+  {
+    "revision": "e27131991baf36999e30255b442693b5",
+    "url": "/algrade/static/media/bgImg1w.e2713199.jpg"
+  },
+  {
+    "revision": "cc8f9466e73992ea4b42dd9329084dda",
+    "url": "/algrade/static/media/bgImg2w.cc8f9466.jpg"
+  },
+  {
+    "revision": "34ca76f592148a898a14e5dab2935050",
+    "url": "/algrade/static/media/bgImg3w.34ca76f5.jpg"
+  },
+  {
+    "revision": "a840325cbd9654b60c3fef682fcc7493",
+    "url": "/algrade/static/media/bgImg4w.a840325c.jpg"
+  },
+  {
+    "revision": "fc5e561aad3b0302e0caffcd7b258395",
+    "url": "/algrade/static/media/couplesRoom.fc5e561a.webp"
+  },
+  {
+    "revision": "7c197385279fdf473dee79083cab82f8",
+    "url": "/algrade/static/media/cruiser.7c197385.png"
+  },
+  {
+    "revision": "5d06d55a1dc316db88858993e949a837",
+    "url": "/algrade/static/media/dubai-wave-hotel.5d06d55a.jpg"
+  },
+  {
+    "revision": "ab103ccd5fc5e027be40a5a63cd5b654",
+    "url": "/algrade/static/media/familyApartment.ab103ccd.webp"
+  },
+  {
+    "revision": "afb6fc649e9da4e97a1e4cd31eceeb75",
+    "url": "/algrade/static/media/familyEating.afb6fc64.webp"
+  },
+  {
+    "revision": "cf8ab11f90d618731ebeee71ae59fbc0",
+    "url": "/algrade/static/media/hall.cf8ab11f.webp"
+  },
+  {
+    "revision": "925a4fadd041a12ace06bfd36dcce7cc",
+    "url": "/algrade/static/media/hotelImg2.925a4fad.jpg"
+  },
+  {
+    "revision": "22dbffbac33783e50c92722ba3e4de79",
+    "url": "/algrade/static/media/logoDark.22dbffba.png"
+  },
+  {
+    "revision": "1b67d8ae18cb185ae0a6865ac6fda1aa",
+    "url": "/algrade/static/media/museum.1b67d8ae.jpg"
+  },
+  {
+    "revision": "716bdecba3df0a486c4264661c8a620b",
+    "url": "/algrade/static/media/outside.716bdecb.webp"
+  },
+  {
+    "revision": "3b0a81e0af2bbf60d2743765a2cec540",
+    "url": "/algrade/static/media/playground.3b0a81e0.webp"
+  },
+  {
+    "revision": "d0231dea373df92429d1d2e9a21f3207",
+    "url": "/algrade/static/media/pool.d0231dea.webp"
+  },
+  {
+    "revision": "8e480981097c4ccd837d9a0125489abd",
+    "url": "/algrade/static/media/sauna.8e480981.webp"
+  },
+  {
+    "revision": "e06cdee22ecd55e01383720c4bf6741b",
+    "url": "/algrade/static/media/singleRoom.e06cdee2.webp"
+  }
+]);
